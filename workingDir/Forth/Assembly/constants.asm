f6f210f84b920931ebf53190ccce074a6b994bdd
Nathan Schulzke <nschulzke@gmail.com>
2016-11-01 13:35:02 -0600

    Formatting, made FILELOC a #constant instead of having custom code.

--- workingDir/Forth/Assembly/constants.asm
+++ workingDir/Forth/Assembly/constants.asm
@@ -1,9 +1,3 @@
-#primitive R0 RZ
-{
-						LD			R0,RET_STACK_ADDRESS
-						JSR			PUSH_R0
-						JSR			NEXT
-}
 #primitive DOCOL _DOCOL
 {
 						LEA			R0,DOCOL
@@ -16,16 +10,16 @@
 						JSR			PUSH_R0
 						JSR			NEXT
 }
+#primitive R0 RZ
+{
+						LD			R0,RET_STACK_ADDRESS
+						JSR			PUSH_R0
+						JSR			NEXT
+}
 #primitive S0 SZ
 {
 						LD			R0,DATA_STACK_ADDRESS
 						JSR			PUSH_R0
 						JSR			NEXT
 }
-#primitive FILELOC FILELOC
-{
-						LD			R0,_FILELOC
-						JSR			PUSH_R0
-						JSR			NEXT
-_FILELOC				.FILL		x4000
-}
+#constant FILELOC FILELOC <x4000>
