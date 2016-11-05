26f11f490efdea180806eb64eee500c3d8f1ab82
Nathan Schulzke <nschulzke@gmail.com>
2016-11-05 15:45:15 -0600

    Cleaned up INPUT a lot

--- workingDir/Forth/Assembly/io.asm
+++ workingDir/Forth/Assembly/io.asm
@@ -54,48 +54,19 @@ KEY_R2					.BLKW		1
 						JSR			NEXT
 						
 _INPUT					ST			R7,INPUT_CB
-						
 						LD			R1,var_BIN
-						BRnp		INPUT_file
+						LDR			R0,R1,#0
 
-						JSR			_KEY
-						
-						LD			R1,key_NL
-						ADD			R1,R1,R0
-						BRz			INPUT_cleanup_NL
-						
-						OUT
+						LD			R2,var_KEYECHO
+						BRz			INPUT_noecho						
+						OUT	( This OUT is only called when we're reading from a file. )
+INPUT_noecho			ADD			R1,R1,#1
+
+						ST			R1,var_BIN
 						
 INPUT_cleanup			LD			R7,INPUT_CB
 						RET
 
-INPUT_cleanup_NL		AND			R1,R0,R0
-						LD			R0,key_SPACE
-						OUT
-						AND			R1,R0,R0
-						BRnzp		INPUT_cleanup
-
-						( We only get here if we're reading a file )
-INPUT_file				LDR			R0,R1,#0
-						BRz			INPUT_eof
-						
-						LD			R2,var_KEYECHO
-						BRz			INPUT_noecho
-					
-						OUT	( This OUT is only called when we're reading from a file. )
-						
-INPUT_noecho			ADD			R1,R1,#1
-						ST			R1,var_BIN
-						ST			R1,var_BIN
-						BRnzp		INPUT_cleanup
-						
-INPUT_eof				AND			R1,R1,#0			( set BIN back to 0 if eof )
-						ST			R1,var_BIN	( and we're going to want to read the next key from the keyboard )
-						LD			R0,key_NL
-						NOT			R0,R0
-						ADD			R0,R0,#1
-						BRnzp		INPUT_cleanup
-												
 INPUT_CB				.BLKW		1
 key_SPACE				.FILL		#32
 }
@@ -144,30 +115,4 @@ _PARSE_delim			.BLKW		1
 						JSR			POP_R0		( Base )
 						PUTS					( We're going to trust in null-terminator )
 						JSR			NEXT
-}
-#primitive B. BDOT
-{
-						JSR			POP_R1		( number to show )
-						AND			R3,R3,#0
-						ADD			R3,R3,#1	( initialize the mask )
-						
-DOTB_loop				AND			R0,R1,R3	( mask the number )
-						BRnp		DOTB_one
-						LD			R0,char_ZERO
-						JSR			PUSH_R0		( store the value to be emitted )
-						BRnzp		DOTB_incr
-DOTB_one				LD			R0,char_ZERO
-						ADD			R0,R0,#1
-						JSR			PUSH_R0		( store the value to be emitted )
-DOTB_incr				ADD			R3,R3,R3	( bitshift left )
-						BRnp		DOTB_loop	( if we haven't shifted off the left edge, then repeat )
-( now we have a stack of 16 characters to be emitted, loop through them )
-						ADD			R3,R3,#15	( R3 is zero already, make it the counter )
-DOTB_outloop			JSR			POP_R0
-						OUT
-						ADD			R3,R3,#-1
-						BRzp		DOTB_outloop
-						
-						JSR			NEXT
-char_ZERO				.FILL		#48
 }
